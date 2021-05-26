63fb0bf467676b51996e2d7d952f00036b24e427
Nikolay Beketov <nbek@miko.ru>
2021-05-26 11:55:22 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: MIKOPBX/ModuleBitrix24Integration
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulebitrix24integration/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,43 @@
 <?php
+return [
+    'mod_b24_i_UpdateStatus' => 'სტატუსის მიღება',
+    'mod_b24_i_Disconnected' => 'გათიშულია Bitrix24– დან',
+    'mod_b24_i_Connected' => 'დაკავშირებულია Bitrix24– თან',
+    'mod_b24_i_ExportRecords' => 'ატვირთეთ საუბარში საუბრების ჩანაწერების ასლები',
+    'mod_b24_i_NotifyOnCall' => 'ზარის დროს გახსენით კლიენტი / გარიგების ბარათი',
+    'mod_b24_i_Auth' => 'შედით Bitrix24– ზე',
+    'mod_b24_i_CallToken' => 'Bitrix24 პორტალზე გამავალი ვებ-ჰოკის პარამეტრებიდან ავტორიზაციის კოდი (ჟეტონის)',
+    'mod_b24_i_Secret' => 'განაცხადის საიდუმლო კოდი',
+    'mod_b24_i_ClientID' => 'კლიენტის განაცხადის ID',
+    'mod_b24_i_PortalUrl' => 'Bitrix24 პორტალის მისამართი',
+    'mod_b24_i_RefreshToken' => 'სიმბოლოს განახლება',
+    'mod_b24_i_ValidateClientRefreshTokenEmpty' => 'სიმბოლოს განახლების პროგრამა არ არის შევსებული',
+    'mod_b24_i_ValidateClientSecretEmpty' => 'საიდუმლო განაცხადის კოდი არ არის შევსებული',
+    'mod_b24_i_ValidateClientIDEmpty' => 'განაცხადის კლიენტის ID არ არის შევსებული',
+    'mod_b24_i_ValidatePortalEmpty' => 'Bitrix24 პორტალის მისამართი არ არის შევსებული',
+    'SubHeaderModuleBitrix24Integration' => 'საშუალებას გაძლევთ დაურეკოთ Bitrix24– დან და აჩვენოთ კლიენტის ბარათი შემომავალი ზარისას',
+    'BreadcrumbModuleBitrix24Integration' => 'ინტეგრაცია CRM Bitrix24– თან',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 5 2020
+ */
+    'repModuleBitrix24Integration' => 'ინტეგრაციის მოდული CRM Bitrix24– თან -% წარმოადგენს%',
+    'mod_b24_i_NoSettings' => 'მოდული არ არის კონფიგურირებული',
+    'mod_b24_i_EnterSearch' => 'ძებნა',
+    'mod_b24_i_ColumnDidAlias' => 'რიცხვების მეტსახელები (DID)',
+    'mod_b24_i_ColumnNumber' => 'Ტელეფონის ნომერი',
+    'mod_b24_i_ColumnName' => 'ხაზის სახელი',
+    'mod_b24_i_AddNewRecord' => 'ხაზის დამატება',
+    'mod_b24_i_ExternalLines' => 'გარე ხაზები',
+    'mod_b24_i_QaGroupRussian' => 'კითხვები, პრობლემების გადაჭრა, ფორუმი',
+    'mod_b24_i_WebinarLinkRussian' => 'ვებინარი - ეტაპობრივი დაყენება',
+    'mod_b24_i_WikiDocsRussian' => 'დოკუმენტაცია',
+    'mod_b24_i_Docs' => 'გამოსადეგი ბმულები',
+    'mod_b24_i_DisableAll' => 'ყველას გამორთეთ',
+    'mod_b24_i_EnableAll' => 'ყველას ჩათვლით',
+    'mod_b24_i_UsersFilter' => 'ფილტრაცია მომხმარებლების მიერ',
+    'mod_b24_i_GeneralSettings' => 'ძირითადი პარამეტრები',
+    'mod_b24_i_StatusError' => 'მოდულის მუშაობის შეცდომა',
+];
